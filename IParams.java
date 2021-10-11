844c00639542dc0bdc44bd75af65dd8726b3f059
zyy <zyy@qq.com>
2021-10-11 22:05:32 +0800

    first

---
+++ IParams.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public interface IParams {
+    // 获取大车位
+    public int getBig();
+    // 获取中车位
+    public int getMedium();
+    // 获取小车位
+    public int getSmall();
+    // 获取停车序列，例如 [1 2 2 3] 表示 依次停一辆大车，中车，中车，小车
+    public ArrayList<Integer> getPlanParking();
+
+}
